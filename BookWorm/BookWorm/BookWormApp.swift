26557af6c7ac3195e2c0fc72acb8ae3b33e5af20
Sreekutty Maya <sreekuttystar@gmail.com>
2024-04-28 21:52:11 +0400

    project setup added

---
+++ BookWorm/BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Sreekutty Maya on 28/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
