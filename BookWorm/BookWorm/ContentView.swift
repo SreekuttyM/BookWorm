6d9555f5515585863bdc58adb624f938e42a9f06
Sreekutty Maya <sreekuttystar@gmail.com>
2024-04-29 14:32:38 +0400

    applying query sort

--- BookWorm/BookWorm/ContentView.swift
+++ BookWorm/BookWorm/ContentView.swift
@@ -10,8 +10,13 @@ import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
-    @Query var books: [Book]
-
+//    @Query var books: [Book]
+//    @Query(sort: \Book.rating, order: .reverse) var books: [Book]
+//    @Query(sort: \Book.title) var books: [Book]
+    @Query(sort: [
+        SortDescriptor(\Book.title),
+        SortDescriptor(\Book.author)
+    ]) var books: [Book]
     @State private var showingAddScreen = false
     
     var body: some View {
