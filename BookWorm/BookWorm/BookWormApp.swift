73d0d42754a5de440162b7b9751d71ee173a424a
Sreekutty Maya <sreekuttystar@gmail.com>
2024-04-29 13:28:44 +0400

    Addbookview  view added with data saved using swift Data

--- BookWorm/BookWorm/BookWormApp.swift
+++ BookWorm/BookWorm/BookWormApp.swift
@@ -6,14 +6,13 @@
 //
 
 import SwiftUI
-
+import SwiftData
 @main
 struct BookWormApp: App {
 
     var body: some Scene {
         WindowGroup {
             ContentView()
-        }
-
+        }.modelContainer(for: Book.self)
     }
 }
